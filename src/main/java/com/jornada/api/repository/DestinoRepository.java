8cc36769b7d40dee45c6f09aadbd311293cbadf1
cleberpereiradev <cleberpereiraeng@gmail.com>
2024-05-15 19:11:17 -0300

    <feat> change findByEstacaoRecomendada implementation

--- src/main/java/com/jornada/api/repository/DestinoRepository.java
+++ src/main/java/com/jornada/api/repository/DestinoRepository.java
@@ -1,8 +1,7 @@
 package com.jornada.api.repository;
 
-import com.jornada.api.dto.depoimentos.DadosListagemDepoimento;
+
 import com.jornada.api.dto.destinos.DadosListagemDestinoAleatorio;
-import com.jornada.api.dto.destinos.DadosListagemDestinoCompleto;
 import com.jornada.api.entity.Destino;
 import com.jornada.api.entity.enums.Estacoes;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +11,7 @@ import java.util.List;
 
 public interface DestinoRepository extends JpaRepository<Destino, Long> {
 
+    @Query("select d from Destino d where d.estacaoRecomendada = :estacaoAtual order by rand() limit 1")
     List<Destino> findByEstacaoRecomendada(Estacoes estacaoAtual);
 
     @Query("select new com.jornada.api.dto.destinos.DadosListagemDestinoAleatorio(d.id, d.nome) from Destino d order by rand() limit 3")
